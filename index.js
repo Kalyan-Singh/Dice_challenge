d8fec37be45c327d9822f178b1729ee6547b4ef6
Kalyan-Singh <99746258+Kalyan-Singh@users.noreply.github.com>
2022-04-10 22:14:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,46 +6,46 @@ function game(){
     var y=Math.floor(Math.random()*6)+1;
 
     if (x==1){
-        document.getElementById("d1").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice1.png";
+        document.getElementById("d1").src="/images/dice1.png";
     }
     if (x==2){
-        document.getElementById("d1").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice2.png";
+        document.getElementById("d1").src="/images/dice2.png";
     }
     if (x==3){
-        document.getElementById("d1").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice3.png";
+        document.getElementById("d1").src="/images/dice3.png";
     }
     if (x==4){
-        document.getElementById("d1").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice4.png";
+        document.getElementById("d1").src="/images/dice4.png";
     }
     if (x==5){
-        document.getElementById("d1").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice5.png";
+        document.getElementById("d1").src="/images/dice5.png";
     }
     if (x==6){
-        document.getElementById("d1").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice6.png";
+        document.getElementById("d1").src="/images/dice6.png";
     }
     if (y==1){
-        document.getElementById("d2").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice1.png";
+        document.getElementById("d2").src="/images/dice1.png";
     }
     if (y==2){
-        document.getElementById("d2").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice2.png";
+        document.getElementById("d2").src="/images/dice2.png";
     }
     if (y==3){
-        document.getElementById("d2").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice3.png";
+        document.getElementById("d2").src="/images/dice3.png";
     }
     if (y==4){
-        document.getElementById("d2").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice4.png";
+        document.getElementById("d2").src="/images/dice4.png";
     }
     if (y==5){
-        document.getElementById("d2").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice5.png";
+        document.getElementById("d2").src="/images/dice5.png";
     }
     if (y==6){
-        document.getElementById("d2").src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/dice6.png";
+        document.getElementById("d2").src="/images/dice6.png";
     }
     if (x>y){
         var heading = document.querySelector(".container h1");
         heading.innerHTML="<em>Player1 wins!</em>";
         var img=new Image();
-        img.src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/flag.jpeg";
+        img.src="/images/flag.jpeg";
         img.width=120;
         var parent=document.querySelector(".container h1");
         parent.prepend(img);
@@ -54,7 +54,7 @@ function game(){
         var heading = document.querySelector(".container h1");
         heading.innerHTML="<em>Player2 wins!  </em>";
         var img=new Image();
-        img.src="/home/kalyan/Web_Dev_udemy/BOSS_LEVEL1/Dicee Challenge - Starting Files/images/flag.jpeg";
+        img.src="/images/flag.jpeg";
         img.width=120;
         var parent=document.querySelector(".container h1");
         parent.append(img);
